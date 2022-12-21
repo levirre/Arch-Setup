93006c2f44190f57d3dd24e4706b122a0a13b91d
Oenna <krischeval@outlook.com>
2022-12-20 18:09:18 -0800

    packages and scripts

---
+++ gitsetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#generate keygen
+set -x
+git config --global user.name "Oenna" &
+git config --global user.email krischeval@outlook.com &
+#git config user.name &
+#git config user.email &
+#bash ssh-keygen -t ed25519 -C "krischeval@outlook.com" &
